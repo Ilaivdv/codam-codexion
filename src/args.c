87b20c8b2223f7386ddfeb6ca8ee9f737aca7ef9
Ilai Van Der Veldt <ivan-der@f0r5s18.codam.nl>
2026-08-18 16:16:39 +0200

    fix(args): change time related args to be in milliseconds

--- src/args.c
+++ src/args.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       ::::::::             */
-/*   args.c                                            :+:    :+:             */
-/*                                                    +:+                     */
-/*   By: ivan-der <ivan-der@student.codam.nl>        +#+                      */
-/*                                                  +#+                       */
-/*   Created: 2026/08/10 14:32:00 by ivan-der     #+#    #+#                  */
-/*   Updated: 2026/08/11 10:40:57 by ivan-der     ########   odam.nl          */
+/*                                                      ::::    :::           */
+/*   args.c                                            :+:+:   :+:            */
+/*                                                    :+:+:+  +:+             */
+/*   By: ivan-der <ivan-der@student.codam.nl>        +#+ +:+ +#+              */
+/*                                                  +#+  +#+#+#               */
+/*   Created: 2026/08/18 15:27:50 by ivan-der      #+#   #+#+#                */
+/*   Updated: 2026/08/18 16:11:06 by ivan-der     ###    #### orminette :(    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <stdio.h>
+
 int	get_args(char **argv, t_ctx *ctx)
 {
 	ctx->coders = atoi(argv[1]);
-	ctx->burnout_time = atoi(argv[2]);
-	ctx->compile_time = atoi(argv[3]);
-	ctx->debug_time = atoi(argv[4]);
-	ctx->refactor_time = atoi(argv[5]);
+	ctx->burnout_time = atoi(argv[2]) * 1000;
+	ctx->compile_time = atoi(argv[3]) * 1000;
+	ctx->debug_time = atoi(argv[4]) * 1000;
+	ctx->refactor_time = atoi(argv[5]) * 1000;
 	ctx->max_compiles = atoi(argv[6]);
-	ctx->dongle_cooldown = atoi(argv[7]);
+	ctx->dongle_cooldown = atoi(argv[7]) * 1000;
 	if (!strcmp(argv[8], "fifo"))
 		ctx->scheduler = FIFO;
 	else if (!strcmp(argv[8], "edf"))
