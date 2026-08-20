63e10f377cf9e5533493097c41179501f251d66c
Ilai Van Der Veldt <ivan-der@f0r3s20.codam.nl>
2026-08-20 12:31:02 +0200

    feat(monitor): add get_elapsed_time function

--- src/main.c
+++ src/main.c
@@ -6,13 +6,15 @@
 /*   By: ivan-der <ivan-der@student.codam.nl>        +#+ +:+ +#+              */
 /*                                                  +#+  +#+#+#               */
 /*   Created: 2026/08/18 15:10:02 by ivan-der      #+#   #+#+#                */
-/*   Updated: 2026/08/20 09:33:01 by ivan-der     ###    #### orminette :(    */
+/*   Updated: 2026/08/20 12:30:35 by ivan-der     ###    #### orminette :(    */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 #include <stdbool.h>
 #include <stdio.h>
+
+#include <unistd.h>
 
 int	main(int argc, char **argv)
 {
@@ -24,6 +26,8 @@ int	main(int argc, char **argv)
 		fprintf(stderr, "Incorrect arguments!\nSee 'make help'\n");
 		return (1);
 	}
+	ctx.params = &params;
+	get_elapsed_time();
 	init_coders(&ctx);
 	// TODO cleanup function here, free and destroy mutexes
 	return (0);
--- src/monitor.c
+++ src/monitor.c
@@ -6,19 +6,23 @@
 /*   By: ivan-der <ivan-der@student.codam.nl>        +#+ +:+ +#+              */
 /*                                                  +#+  +#+#+#               */
 /*   Created: 2026/08/19 12:13:09 by ivan-der      #+#   #+#+#                */
-/*   Updated: 2026/08/19 22:39:27 by ivan-der     ###    #### orminette :(    */
+/*   Updated: 2026/08/20 12:29:57 by ivan-der     ###    #### orminette :(    */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../include/codexion.h"
-// #include <sys/time.h>
-// #include <unistd.h>
+#include <stddef.h>
+#include <sys/time.h>
 
 // TODO change this to use a static var for start time for reusability
-// unsigned int	get_elapsed_time(t_ctx *ctx)
-// {
-// 	struct timeval	tv;
-//
-// 	gettimeofday(&tv, NULL);
-// 	return ((tv.tv_usec - ctx->monitor->epoch) / 1000);
-// }
+long long	get_elapsed_time(void)
+{
+	static long long	epoch = -1;
+	struct timeval		tv;
+	long long			current_time;
+
+	gettimeofday(&tv, NULL);
+	if (epoch == -1)
+		epoch = tv.tv_usec;
+	current_time = tv.tv_usec;
+	return ((current_time - epoch) / 1000);
+}
